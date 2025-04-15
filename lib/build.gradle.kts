8710472f969af0212a530abe8c7be382ea620ba3
Nijat Aliyev <nicat3699@gmail.com>
2025-04-15 15:19:00 +0400

    Update build.gradle.kts

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -2,13 +2,16 @@ plugins {
     `java-library`
 }
 
+group = "az.arvilo"
+version = "1.0.0"
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    api(libs.commons.math3)
-    implementation(libs.guava)
+    api("org.apache.commons:commons-math3:3.6.1")
+    implementation("com.google.guava:guava:32.1.2-jre")
 }
 
 java {
